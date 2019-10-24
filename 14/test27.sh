805861b17ba146ebc5684706b73c5def19cfc0b3
root <root@node1.test.com>
2019-10-24 11:24:27 +0800

    final all 14

---
+++ 14/test27.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+read -s -p "Enter your password: " pass
+
+echo 
+echo "Is your password really $pass? "
+
---
+++ 14/test28.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+count=1
+cat test | while read line;do	
+	echo "Line $count: $line"
+	count=$[ $count + 1 ]
+done
+
+echo "Finished processing the file"
+
+
